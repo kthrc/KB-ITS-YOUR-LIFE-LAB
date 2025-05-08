4f488323674ab3a683095d68d9642a35ee34bc3b
Yujung Choi <yujung5548@naver.com>
2025-05-09 00:59:43 +0900

    Add: 10_java(심화)

---
+++ fullstack/back-end/10_java(심화)/10th_adv/src/ch15/sec05/exam04/Fruit.java
@@ -0,0 +1,13 @@
+package ch15.sec05.exam04;
+
+public class Fruit {
+    public String name;
+    public int price;
+
+    // 전체 매개변수 생성자 추가
+
+    public Fruit(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
